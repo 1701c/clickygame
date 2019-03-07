2cb66569fb4ffcf056bbdcbc7d699c36f525b71a
1701c <alex@reventlow.net>
2019-03-06 19:13:51 -0600

    eslintrc

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Card from "./components/Card";
-// import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
 import cards from "./cards.json";
 import "./App.css";
@@ -12,31 +11,6 @@ class App extends Component {
     highscore: 0,
     message: 'Click an Image to Begin'
   };
-
-  // newGame = () => {
-  //   this.setState({ score: 0 });
-  //   this.state.cards.forEach(card => { card.count = 0; })
-  // }
-
-  // cardClicked = id => {
-  //   this.state.cards.find((cardToMatch, i) => {
-  //     if (cardToMatch.id === id) {
-  //       if (cards[i].count === 0) {
-  //         cards[i].count = cards[i].count + 1;
-  //         this.setState({ score: this.state.score + 1, message: "Correct!" }, function () {
-  //           if (this.state.score > this.state.highscore) {
-  //             this.setState({ highscore: this.state.score });
-  //             if (this.state.score === 12) this.setState({ message: "You Win!"});
-  //           };
-  //         });
-  //         this.state.cards.sort(() => Math.random() - .5)
-  //       } else {
-  //         this.setState({ message: "You Lose!" });
-  //         this.newGame();
-  //       }
-  //     } return null;
-  //   });
-  // }
 
   newGame = () => {
     this.setState({ score: 0 });
--- src/components/Card.js
+++ src/components/Card.js
@@ -9,14 +9,8 @@ const Card = props => (
 );
 
 const cardStyle = {
-  // background: "#fff",
-  // borderRadius: "10px",
   height: "275px",
-  // margin: "1rem",
-  // position: "relative",
   width: "275px",
-  // boxShadow: "0 3px 6px #999, 0 3px 6px #999",
-  // textAlign: "center",
   float: "left"
 }
 
@@ -24,11 +18,8 @@ const imgContainerStyle = {
   height: "100%",
   overflow: "hidden",
   textAlign: "center",
-  
-    // marginRight: "auto",
   backgroundImage: "url('https://vignette.wikia.nocookie.net/youtubepoop/images/4/4c/Pokeball.png/revision/latest?cb=20150418234807')",
   backgroundSize: "100%"
-  // background: "#6CADDC"
 }
 
 const imgStyle  = {
