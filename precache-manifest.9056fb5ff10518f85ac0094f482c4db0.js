2607b77fdc9692d43170ad4bc4dbf217c17b324c
1701c <alex@reventlow.net>
2019-03-06 19:39:06 -0600

    Updates

---
+++ precache-manifest.9056fb5ff10518f85ac0094f482c4db0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "601067f922f7ef301ffb",
+    "url": "/1701c/clickygame/static/js/runtime~main.601067f9.js"
+  },
+  {
+    "revision": "ebbee6292bc4254304b4",
+    "url": "/1701c/clickygame/static/js/main.ebbee629.chunk.js"
+  },
+  {
+    "revision": "ff661de6195320cfc0eb",
+    "url": "/1701c/clickygame/static/js/2.ff661de6.chunk.js"
+  },
+  {
+    "revision": "ebbee6292bc4254304b4",
+    "url": "/1701c/clickygame/static/css/main.2705a57e.chunk.css"
+  },
+  {
+    "revision": "c5190025411f44b6c4dd659e91fc9da1",
+    "url": "/1701c/clickygame/index.html"
+  }
+];
